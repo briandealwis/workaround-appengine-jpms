3aa98a59798c1e358556ba502b87683bd4608519
Brian de Alwis <bsd@mt.ca>
2017-08-30 16:00:42 -0400

    Simple AESv8 project

---
+++ README.md
@@ -0,0 +1,6 @@
+Starting project, generated from the Cloud Tools for Eclipse _New Google
+App Engine standard project_ for the Java 8 environment.
+
+To build:
+
+  1. `cd example-consumer-war && mvn clean appengine:run`
---
+++ example-consumer-war/src/main/java/com/example/yasson/HelloAppEngine.java
@@ -0,0 +1,17 @@
+package com.example.yasson;
+
+import java.io.IOException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloAppEngine extends HttpServlet {
+
+  public void doGet(HttpServletRequest request, HttpServletResponse response) 
+      throws IOException {
+      
+    response.setContentType("text/plain");
+    response.getWriter().print("Hello App Engine!\r\n");
+
+  }
+}
